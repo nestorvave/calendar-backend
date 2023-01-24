c621f90814d043944b1b1e8e7e86f1d3744c5f54
nestorvave <84485292+nestorvave@users.noreply.github.com>
2023-01-24 00:06:52 -0600

    db configurated successful

--- routes/auth.js
+++ routes/auth.js
@@ -4,17 +4,35 @@
  */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   createUser,
   loginUser,
   revalidateToken,
 } = require("../controllers/auth");
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
 
-router.post("/register", createUser);
+router.post(
+  "/register",
+  [
+    //middleware
+    check("name", "Elname es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe tener 6 caracteres").isLength({
+      min: 6,
+    }),
+    validateFields
+  ],
+  createUser
+);
 
-router.post("/", loginUser);
+router.post("/", [
+  check("email", "Debe contener un email válido").isEmail(),
+  check("password", "Debe contener al menos 6 letras").isLength({ min: 6 }),
+  validateFields
+], loginUser);
 
 router.get("/renew", revalidateToken);
 
